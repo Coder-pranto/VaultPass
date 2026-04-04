d6c989be84594fbe3133f895117db7afe3234dfa
Riaj Hasan Pranto <prantocse18@gmail.com>
2026-04-04 11:47:05 +0600

    chore: add package.json with initial dependencies and scripts

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
+require('dotenv').config({ quiet: true });
+require('colors');
+
 const express = require('express');
+const path = require('path');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const colors = require('colors');
-const connectDB = require('./config/db');
 
+const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const PORT = process.env.PORT || 5000;
-
-dotenv.config({ debug: true });
 
 const app = express();
 
@@ -18,13 +20,37 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin:
+      process.env.NODE_ENV === 'production'
+        ? 'https://vaultpass.onrender.com'
+        : 'http://localhost:5173',
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
   }),
 );
 
 // routes
 app.use('/api/auth', authRoutes);
+
+
+// PRODUCTION SETUP
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  });
+}
+
+
+// 404 handler
+app.use((req, res, next) => {
+  res.status(404).json({
+    success: false,
+    message: `Route not found: ${req.originalUrl}`,
+  });
+});
+
 
 // global error handling middleware
 app.use((err, req, res, next) => {
